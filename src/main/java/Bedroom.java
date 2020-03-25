917c06cdc6cd821ac12bb973e889114c712b8fa8
mholwill <m_holwill@hotmail.co.uk>
2020-03-25 14:04:12 +0000

    Created bedroom class

--- src/main/java/Bedroom.java
+++ src/main/java/Bedroom.java
@@ -1,2 +1,22 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Bedroom {
+
+    private int roomNumber;
+    private int capacity;
+    private String type;
+    private ArrayList<Guest> guests;
+
+    public Bedroom(int roomNumber, int capacity, String type){
+        this.roomNumber = roomNumber;
+        this.capacity = capacity;
+        this.type = type;
+        this.guests = new ArrayList<Guest>();
+    }
+
+
+    public int getGuests() {
+        return this.guests.size();
+    }
 }
--- src/test/java/BedroomTest.java
+++ src/test/java/BedroomTest.java
@@ -1,5 +1,33 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class BedroomTest {
 
-    Bedroom bedroom;
+    private Bedroom bedroom;
+    private Guest guest1;
+
+    @Before
+    public void setUp() {
+        bedroom = new Bedroom(3, 2, "double");
+        guest1 = new Guest("Malcolm");
+    }
+
+    //check room empty
+    @Test
+    public void checkRoomEmpty() {
+        assertEquals(0, bedroom.getGuests());
+    }
+
+    // add guest
+
+
+    // remove guest
+
+
+    //check capacity
+
+
 
 }

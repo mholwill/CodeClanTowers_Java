2dde76302cd7b18d69d3c4deeaf4aac0f04cb85d
mholwill <m_holwill@hotmail.co.uk>
2020-03-25 14:33:07 +0000

    Conference room methods complete

--- src/main/java/ConferenceRoom.java
+++ src/main/java/ConferenceRoom.java
@@ -1,2 +1,39 @@
+import java.util.ArrayList;
+
 public class ConferenceRoom {
+
+    private String name;
+    private int capacity;
+    private ArrayList<Guest> guests;
+
+    public ConferenceRoom(String name, int capacity){
+        this.name = name;
+        this.capacity = capacity;
+        this.guests = new ArrayList<Guest>();
+    }
+
+    public int getGuests() {
+        return this.guests.size();
+    }
+
+    public int getCapacity() {
+        return this.capacity;
+    }
+
+    public boolean hasCapacity() {
+        return this.capacity > this.guests.size();
+    }
+
+    public void addGuest(Guest guest) {
+        if (hasCapacity()) {
+            this.guests.add(guest);
+        }
+    }
+
+    public void removeGuest(Guest guest) {
+        if (this.guests.contains(guest)){
+            this.guests.remove(guest);
+        }
+    }
+
 }
--- src/test/java/ConferenceRoomTest.java
+++ src/test/java/ConferenceRoomTest.java
@@ -1,2 +1,41 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class ConferenceRoomTest {
+
+    private ConferenceRoom conferenceRoom;
+    private Guest guest1;
+
+    @Before
+    public void setUp() {
+        conferenceRoom = new ConferenceRoom("Hopper", 50);
+        guest1 = new Guest("Graham");
+    }
+
+    @Test
+    public void checkRoomEmpty() {
+        assertEquals(0, conferenceRoom.getGuests());
+    }
+
+    // add guest
+    @Test
+    public void checkAddGuestToRoom() {
+        conferenceRoom.addGuest(guest1);
+        assertEquals(1, conferenceRoom.getGuests());
+    }
+
+    // remove guest
+    @Test
+    public void checkRemoveGuestFromRoom(){
+        conferenceRoom.addGuest(guest1);
+        conferenceRoom.removeGuest(guest1);
+        assertEquals(0, conferenceRoom.getGuests());
+    }
+
+    @Test
+    public void checkCapacity(){
+        assertEquals(50, conferenceRoom.getCapacity());
+    }
 }

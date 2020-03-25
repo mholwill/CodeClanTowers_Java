0a4d868263137dc2d8d0aaee30fb6852347cf9fd
mholwill <m_holwill@hotmail.co.uk>
2020-03-25 15:42:37 +0000

    Hotel MVP complete

--- src/main/java/Hotel.java
+++ src/main/java/Hotel.java
@@ -13,10 +13,53 @@ public class Hotel {
     }
 
 
-    public boolean checkAllGuestsFireAlarm() {
-        if (this.bedrooms.size() == 0 && this.conferenceRooms.size() == 0) {
-            return true;
-        }
-        return false;
+//    public boolean checkAllGuestsFireAlarm() {
+//        if (this.bedrooms.guest.size() && this.conferenceRooms.contains(0)) {
+//            return true;
+//        }
+//        return false;
+//    }
+
+    public void addBedroom(Bedroom bedroom) {
+        this.bedrooms.add(bedroom);
+    }
+
+
+    public int getBedrooms() {
+        return this.bedrooms.size();
+    }
+
+    public void addConferenceRoom(ConferenceRoom conferenceRoom) {
+        this.conferenceRooms.add(conferenceRoom);
+    }
+
+    public int getConferenceRooms() {
+        return this.conferenceRooms.size();
+    }
+
+
+    public int guestsInBedroom(Bedroom bedroom) {
+        return bedroom.getGuests();
+    }
+
+    public void checkInGuestToBedroom(Guest guest, Bedroom bedroom) {
+        bedroom.addGuest(guest);
+    }
+
+
+    public void checkOutGuestFromBedroom(Guest guest, Bedroom bedroom) {
+        bedroom.removeGuest(guest);
+    }
+
+    public int guestsInConferenceRoom(ConferenceRoom conferenceRoom){
+        return conferenceRoom.getGuests();
+    }
+
+    public void checkInGuestToConferenceRoom(Guest guest, ConferenceRoom conferenceRoom) {
+        conferenceRoom.addGuest(guest);
+    }
+
+    public void checkOutGuestFromConferenceRoom(Guest guest, ConferenceRoom conferenceRoom) {
+        conferenceRoom.removeGuest(guest);
     }
 }
--- src/test/java/HotelTest.java
+++ src/test/java/HotelTest.java
@@ -23,6 +23,12 @@ public class HotelTest {
         bedroom2 = new Bedroom(3, 2, "double");
         conferenceRoom1 = new ConferenceRoom("Hopper", 3);
         conferenceRoom2 = new ConferenceRoom("Corona", 6);
+
+        hotel.addBedroom(bedroom1);
+        hotel.addBedroom(bedroom2);
+        hotel.addConferenceRoom(conferenceRoom1);
+        hotel.addConferenceRoom(conferenceRoom2);
+
         guest1 = new Guest("Alan");
         guest2 = new Guest("Malcolm");
         guest3 = new Guest("Louise");
@@ -31,19 +37,56 @@ public class HotelTest {
     }
 
     //checkAllGuestsInHotel
+//    @Test
+//    public void checkAllGuestsInHotel(){
+//        assertEquals(true, hotel.checkAllGuestsFireAlarm());
+//    }
+
     @Test
-    public void checkAllGuestsInHotel(){
-        assertEquals(true, hotel.checkAllGuestsFireAlarm());
+    public void addBedroomToHotel(){
+        assertEquals(2, hotel.getBedrooms());
     }
 
-    //checkGuestInToBedroom
+    @Test
+    public void addConferenceRoomToHotel(){
+        assertEquals(2, hotel.getConferenceRooms());
+    }
+
+    @Test
+    public void checkInGuestToBedroom(){
+        hotel.checkInGuestToBedroom(guest1, bedroom1);
+        assertEquals(1, hotel.guestsInBedroom(bedroom1));
+    }
+
+    @Test
+    public void cantCheckGuestInToBedroom(){
+        hotel.checkInGuestToBedroom(guest1, bedroom1);
+        hotel.checkInGuestToBedroom(guest2, bedroom1);
+        assertEquals(1, hotel.guestsInBedroom(bedroom1));
+    }
 
     //checkGuestOutOfBedroom
+    @Test
+    public void checkGuestOutOfBedroom(){
+        hotel.checkInGuestToBedroom(guest1, bedroom1);
+        hotel.checkOutGuestFromBedroom(guest1, bedroom1);
+        assertEquals(0, hotel.guestsInBedroom(bedroom1));
+    }
 
     //checkGuestInToConferenceRoom
+    @Test
+    public void checkInGuestToConfrenceRoom(){
+        hotel.checkInGuestToConferenceRoom(guest1, conferenceRoom1);
+        assertEquals(1, hotel.guestsInConferenceRoom(conferenceRoom1));
+    }
 
     //checkGuestOutOfConferenceRoom
-
+    @Test
+    public void checkGuestOutOfConferenceRoom(){
+        hotel.checkInGuestToConferenceRoom(guest1, conferenceRoom1);
+        hotel.checkOutGuestFromConferenceRoom(guest1, conferenceRoom1);
+        assertEquals(0, hotel.guestsInConferenceRoom(conferenceRoom1));
+    }
 
 
 
